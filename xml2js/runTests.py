966f5b68659a6b34b7b45f100aa9fb588efa281f
vostruk <vovaostruki@gmail.com>
2016-01-14 20:35:01 +0100

    Cleaning up

--- xml2js/runTests.py
+++ xml2js/runTests.py
@@ -1,8 +1,22 @@
 import json
 import os
 import subprocess
+programPath = os.getcwd()
 program = "xml2js.exe"
-for file in os.listdir("Tests"):
+filesPath = "Tests"
+print("\n---- Stage 1 ----\nGenerating JSON files from XML files:")
+for file in os.listdir(filesPath):
     if file.endswith(".xml"):
-        subprocess.call([program, file])
-		print(file)
+        subprocess.call([program, filesPath+"/"+file])
+        print(file +".json generated")
+print("\n----Stage 2 ----\nChecking if JSON files are valid:")
+for file in os.listdir(filesPath):
+    if file.endswith(".json"):
+        with open(filesPath+'/'+file, 'r') as myfile:
+            data=myfile.read()
+            try:
+                json.loads(data)
+                print("JSON file "+file+" is OK")
+            except ValueError as e:
+                print('invalid json file '+file+ ", error: %s" %e)
+input("Press Enter to continue...")
